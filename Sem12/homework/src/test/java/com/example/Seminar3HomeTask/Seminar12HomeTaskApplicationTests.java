bc47bb29dfb4b436c8534689276d27c591981ea7
Mipusatina <mipspam2@mail.ru>
2024-04-13 12:41:24 +0200

    Sem 12. DZ

---
+++ Sem12/homework/src/test/java/com/example/Seminar3HomeTask/Seminar12HomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Seminar3HomeTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar12HomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
